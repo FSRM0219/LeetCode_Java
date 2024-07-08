a792ca6015d17b750c574b86c6909c80f8270b62
XPRJ0219 <fsrm0219@qq.com>
2024-07-08 17:29:24 +0800

    2024.7.8

--- src/App.java
+++ src/App.java
@@ -1,10 +1,12 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        System.out.println(num);
-        System.out.print("Hello, World!");
+    public static void main(String[] args) {
+        Random rm = new Random();
+        for (int i = 0; i < 6; i++) {
+            int num = rm.nextInt(3);
+            System.out.printf("%d ", num);
+        }
     }
 }
