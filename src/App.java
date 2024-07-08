41e0f5a72002fea5f6579b5ab6c01be1511d496a
XPRJ0219 <fsrm0219@qq.com>
2024-07-08 17:32:29 +0800

    2024.7.8

--- src/App.java
+++ src/App.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Random rm = new Random();
-        for (int i = 0; i < 6; i++) {
-            int num = rm.nextInt(3);
-            System.out.printf("%d ", num);
-        }
+        System.out.println("Hello World");
     }
 }
