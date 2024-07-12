32d6c237680ffcd32e436a97b7775357f204dca0
XPRJ0219 <fsrm0219@qq.com>
2024-07-12 09:13:03 +0800

    2024.7.12

--- src/App.java
+++ src/App.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String s = "Hello World";
+        System.out.println(s.length());
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            System.out.println(str[i]);
+        }
+        s.startsWith("h");
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.remove(0);
+        list.remove(1);
     }
 }
