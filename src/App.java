93ab97d1d60c51e8b59a68ebbdcf3a5a0105dee1
XPRJ0219 <fsrm0219@qq.com>
2024-07-12 09:21:20 +0800

    2024.7.12

--- src/App.java
+++ src/App.java
@@ -1,14 +1,10 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
         String s = "Hello World";
         System.out.println(s.length());
         String[] str = s.split(" ");
-        for (int i = 0; i < str.length; i++) {
-            System.out.println(str[i]);
+        for (String string : str) {
+            System.out.println(string);
         }
     }
 }
