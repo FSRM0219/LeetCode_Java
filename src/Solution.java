0c9b9466ed6b0db62fd9653871e708a2e12ef122
XPRJ0219 <fsrm0219@qq.com>
2024-07-13 22:22:06 +0800

    2024.7.13

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,164 @@
 import java.util.*;
 
 public class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(p -> p[0]));
+        List<int[]> ans = new ArrayList<>();
+        int left = intervals[0][0];
+        int right = intervals[0][1];
+        for (int[] interval : intervals) {
+            if (interval[0] <= right) {
+                right = Math.max(right, interval[1]);
+            } else {
+                ans.add(new int[]{left, right});
+                left = interval[0];
+                right = interval[1];
+            }
+        }
+        ans.add(new int[]{left, right});
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+    public int maxSubArray(int[] nums) {
+        int preSum = nums[0];
+        int ans = preSum;
+        for (int i = 1; i < nums.length; i++) {
+            preSum = Math.max(preSum + nums[i], nums[i]);
+            ans = Math.max(ans, preSum);
+        }
+        return ans;
+    }
+
+    public String minWindow(String s, String t) {
+        int[] cntS = new int[128], cntT = new int[128];
+        int ls = s.length();
+        int tt = t.length();
+        for (int i = 0; i < tt; i++) {
+            cntT[t.charAt(i) - 'A']++;
+        }
+        int ansl = -1, ansr = ls;
+        int left = 0;
+        for (int right = 0; right < ls; right++) {
+            cntS[s.charAt(right) - 'A']++;
+            while (isCovered(cntS, cntT)) {
+                if (right - left < ansr - ansl) {
+                    ansr = right;
+                    ansl = left;
+                }
+                cntS[s.charAt(left) - 'A']--;
+                left++;
+            }
+        }
+        if (ansl < 0) {
+            return "";
+        }
+        return s.substring(ansl, ansr + 1);
+    }
+
+    private boolean isCovered(int[] cntS, int[] cntT) {
+        for (int i = 0; i < 128; i++) {
+            if (cntS[i] < cntT[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length - k + 1];
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!list.isEmpty() && nums[i] > nums[list.getLast()]) {
+                list.removeLast();
+            }
+            list.add(i);
+            if (i - list.getFirst() + 1 > k) {
+                list.removeFirst();
+            }
+            if (i + 1 >= k) {
+                ans[i - k + 1] = nums[list.getFirst()];
+            }
+        }
+        return ans;
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int result = 0;
+        int preSum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int num : nums) {
+            preSum += num;
+            if (map.containsKey(preSum - k) && map.get(preSum - k) > 0) {
+                result += map.get(preSum - k);
+            }
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return result;
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int sLen = s.length();
+        int pLen = p.length();
+        if (sLen < pLen) {
+            return new ArrayList<>();
+        }
+        int[] sCount = new int[26];
+        int[] pCount = new int[26];
+        for (int i = 0; i < pLen; i++) {
+            sCount[s.charAt(i) - 'a']++;
+            pCount[p.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(sCount, pCount)) {
+            res.add(0);
+        }
+        for (int i = 0; i < sLen - pLen; i++) {
+            sCount[s.charAt(i) - 'a']--;
+            sCount[s.charAt(i + pLen) - 'a']++;
+            if (Arrays.equals(sCount, pCount)) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int left = 0;
+        int ans = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int right = 0; right < n; right++) {
+            while (left < right && set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            ans = Math.max(ans, right - left + 1);
+            set.add(s.charAt(right));
+        }
+        return ans;
+    }
+
+    public int trap(int[] height) {
+        int ans = 0;
+        int n = height.length;
+        int[] preMax = new int[n];
+        int[] folMax = new int[n];
+        preMax[0] = height[0];
+        folMax[n - 1] = height[n - 1];
+        for (int i = 1; i < n; i++) {
+            preMax[i] = Math.max(preMax[i - 1], height[i]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            folMax[i] = Math.max(folMax[i + 1], height[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            ans += Math.min(preMax[i], folMax[i]) - height[i];
+        }
+        return ans;
+    }
+
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
